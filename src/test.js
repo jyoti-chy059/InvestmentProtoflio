23955dcaed871439e427700d26d7d549090e18af
Jyoti <jyoti.chy059@gmail.com>
2024-05-13 11:32:37 +0800

    Create test.js

---
+++ src/test.js
@@ -0,0 +1,10 @@
+function compareTwoNumber(num1, num2){
+  if(num1 == num2){
+    return true
+  }else if(num1 != num2){
+    return false
+  }
+}
+
+const isNumEqual = compareTwoNumber(10, 12) 
+console.log(isNumEqual)
